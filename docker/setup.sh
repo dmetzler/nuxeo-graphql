bbf403a3c3f202deae95e2b50aab5a9184452a02
Damien Metzler <dmetzler@nuxeo.com>
2016-06-14 15:29:25 +0200

    Check if Nuxeo Package is installed before installing it

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-gosu $NUXEO_USER $NUXEO_HOME/bin/nuxeoctl mp-install /docker-entrypoint-initnuxeo.d/marketplace-nuxeo-graphql-*.zip --relax=false --accept=true
+set +e
 
+gosu $NUXEO_USER $NUXEO_HOME/bin/nuxeoctl mp-list|grep nuxeo-graphql > /dev/null 2>&1
+if [ $? -eq 1 ]; then 
+	gosu $NUXEO_USER $NUXEO_HOME/bin/nuxeoctl mp-install /docker-entrypoint-initnuxeo.d/marketplace-nuxeo-graphql-*.zip --relax=false --accept=true
+fi
+set -e
